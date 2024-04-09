78112b2b6a6f36b27b9deffce109235a23e3f472
dingwd <dingwd@unitechs.com>
2024-04-09 15:53:34 +0800

    fix

--- dtc-infrastructure/dtc-infrastructure-utils/src/main/java/com/dingwd/infrastructure/utils/typeturn/JavaTypeConstant.java
+++ dtc-infrastructure/dtc-infrastructure-utils/src/main/java/com/dingwd/infrastructure/utils/typeturn/JavaTypeConstant.java
@@ -2,7 +2,6 @@ package com.dingwd.infrastructure.utils.typeturn;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @AllArgsConstructor
@@ -21,25 +20,22 @@ public enum JavaTypeConstant {
     DATETIME("DATETIME", "java.time.LocalDateTime"),
     DATE("DATE", "java.time.LocalDateTime"),
     TIME("TIME", "java.time.LocalDateTime"),
-    DATETIMEV2("DATETIMEV2","java.time.LocalDateTime");
+    DATETIMEV2("DATETIMEV2", "java.time.LocalDateTime");
 
     final String dbType;
     final String javaType;
 
     public static String getJavaType(String columnType, String fieldPrecision) {
+        if (INT.dbType.equalsIgnoreCase(columnType) && Integer.parseInt(fieldPrecision) >= 10) {
+            return "Long";
+        }
         JavaTypeConstant type;
         try {
             type = JavaTypeConstant.valueOf(columnType.toUpperCase());
         } catch (Exception e) {
-            return "String";
+            throw new RuntimeException(String.format("没有找到[%s]对应的类型", columnType));
         }
-        if (DECIMAL.dbType.equalsIgnoreCase(columnType)) {
-            return type.getJavaType();
-        } else if (INT.dbType.equalsIgnoreCase(columnType) && Integer.parseInt(fieldPrecision) >= 10) {
-            return "Long";
-        } else {
-            return type.getJavaType();
-        }
+        return type.getJavaType();
     }
 
 }
